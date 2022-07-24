25bbfc0bf8c9d4f78b868275a56aa73c1ff16473
Lucas Pacheco <pacheco_lpg@live.com>
2022-07-24 18:46:03 -0300

    Exercicio 11

--- test/exercicios11_test.dart
+++ test/exercicios11_test.dart
@@ -11,14 +11,36 @@ import 'dart:math';
 
 int a1 = -5;
 int a2 = 5;
+double soma1 = 0;
 
 int b1 = -100;
 int b2 = 10;
+double soma2 = 0;
 
-double f(int x) => (sin(x)) * (cos(x)) * (x + x) - 3;
+double fdex(int x) => (sin(x)) * (cos(x)) * (x + x) - 3;
 
-double g(int x) => -2 * pow(x, 3) - 5 * pow(x, 2) + (6 * x) - 5;
+double gdex(int x) => -2 * pow(x, 3) - 5 * pow(x, 2) + (6 * x) - 5;
 
 void secante() {
-  List<double> x;
+  double f;
+  double g;
+
+  for (int i = a1; i <= a2; i++) {
+    f = ((i - 1) * fdex(i)) - ((i) * fdex(i - 1)) / (fdex(i)) - fdex(i - 1);
+    print("F($i) = $f");
+    soma1 += f;
+  }
+  print("Soma 1: $soma1");
+  print("-------------------------------");
+  for (int i = b1; i <= b2; i++) {
+    g = ((i - 1) * gdex(i)) - ((i) * gdex(i - 1)) / (gdex(i)) - gdex(i - 1);
+    print("G($i) = $g");
+    soma2 += g;
+  }
+
+  print("Soma 2: $soma2");
+}
+
+void main() {
+  secante();
 }
