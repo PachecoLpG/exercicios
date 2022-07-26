e212af7016d6af52fcb60dd378a923be0dc75771
Lucas Pacheco Guarnieri <lucas.pacheco@ravex.com.br>
2022-07-26 14:01:04 -0300

    Wip exercicio 13

--- test/exercicios13_test.dart
+++ test/exercicios13_test.dart
@@ -1,3 +1,6 @@
+@Tags(<String>['ex13'])
+import 'package:test/test.dart';
+
 /// Criar uma classe [Database] com métodos para add [Customer],
 /// [Order] e [Item], além de linkar os objetos pelo id informardo a medida que são add.
 ///
@@ -113,33 +116,39 @@ typedef Json = Map<String, dynamic>;
 ///
 /// `{customers: [{id: 1, name: CustomerName, orders: [{id: 1, items: [{name: ItemName, quantity: 2, unityPrice: 50}], totalPrice: 100}]}]}`
 
-void main(List<String> arguments) {
-  final Database db = Database();
-  db.addCustomer(customer: Customer(id: 1, name: 'Alfa'));
-  db.addCustomer(customer: Customer(id: 2, name: 'Bravo'));
-  // TotalPrice: 250
+void main() {
+  test(
+    '',
+    () {
+      final Database db = Database();
+      db.addCustomer(customer: Customer(id: 1, name: 'Alfa'));
+      db.addCustomer(customer: Customer(id: 2, name: 'Bravo'));
+      // TotalPrice: 250
 
-  db.addOrder(order: Order(id: 1), customerId: 1);
-  db.addItem(
-    item: Item(name: 'Alfa-order1-itemA', quantity: 50, unityPrice: 10),
-    orderId: 1,
+      db.addOrder(order: Order(id: 1), customerId: 1);
+      db.addItem(
+        item: Item(name: 'Alfa-order1-itemA', quantity: 50, unityPrice: 10),
+        orderId: 1,
+      );
+      db.addItem(
+        item: Item(name: 'Alfa-order1-itemB', quantity: 150, unityPrice: 0.2),
+        orderId: 1,
+      );
+      // TotalPrice: 30
+      db.addOrder(order: Order(id: 2), customerId: 1);
+      db.addItem(
+        item: Item(name: 'Alfa-order2-itemA', quantity: 30, unityPrice: 0.3),
+        orderId: 2,
+      );
+      // TotalPrice: 100
+      db.addOrder(order: Order(id: 3), customerId: 2);
+      db.addItem(
+        item: Item(name: 'Bravo-order3-itemA', quantity: 20, unityPrice: 17),
+        orderId: 3,
+      );
+      Json output = db.toJson();
+      print(output);
+    },
+    skip: true,
   );
-  db.addItem(
-    item: Item(name: 'Alfa-order1-itemB', quantity: 150, unityPrice: 0.2),
-    orderId: 1,
-  );
-  // TotalPrice: 30
-  db.addOrder(order: Order(id: 2), customerId: 1);
-  db.addItem(
-    item: Item(name: 'Alfa-order2-itemA', quantity: 30, unityPrice: 0.3),
-    orderId: 2,
-  );
-  // TotalPrice: 100
-  db.addOrder(order: Order(id: 3), customerId: 2);
-  db.addItem(
-    item: Item(name: 'Bravo-order3-itemA', quantity: 20, unityPrice: 17),
-    orderId: 3,
-  );
-  Json output = db.toJson();
-  print(output);
 }
