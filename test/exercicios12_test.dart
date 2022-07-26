2128e97344dfcadabbb16bdac2f0a4a5c51cdaff
Lucas Pacheco Guarnieri <lucas.pacheco@ravex.com.br>
2022-07-26 14:00:45 -0300

    Ajuste exercicio 12

--- test/exercicios12_test.dart
+++ test/exercicios12_test.dart
@@ -14,9 +14,21 @@ enum Color {
 // e outra que retorne a cor anterior na sequência e a defina como atual
 // Considere que a anterior da primeira cor é a última e que a próxima da última é a primeira
 
-Color nextColor(Color color) => Color.values[color.index + 1];
+Color nextColor(Color color) {
+  if (color.index == Color.values.last.index) {
+    return Color.values.first;
+  } else {
+    return Color.values[color.index + 1];
+  }
+}
 
-Color beforeColor(Color color) => Color.values[color.index - 1];
+Color beforeColor(Color color) {
+  if (color.index == Color.values.first.index) {
+    return Color.values.last;
+  } else {
+    return Color.values[color.index - 1];
+  }
+}
 
 void main() {
   test('deve mudar para proxima cor', () {
@@ -37,5 +49,19 @@ void main() {
     color = beforeColor(color);
 
     expect(color, Color.yellow);
+  });
+
+  test('deve mudar para a cor correta', () {
+    Color color = Color.red;
+    Color color2 = Color.purple;
+
+    expect(color, Color.red);
+    expect(color2, Color.purple);
+
+    color = beforeColor(color);
+    color2 = nextColor(color);
+
+    expect(color, Color.purple);
+    expect(color2, Color.red);
   });
 }
